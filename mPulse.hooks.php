bd7bb6195229c2d2861856dddc73a5898ca12a4d
Nic Jansma <nic@nicj.net>
2017-12-20 09:12:33 -0500

    mPulse Loader Snippet v10

--- mPulse.hooks.php
+++ mPulse.hooks.php
@@ -13,12 +13,20 @@ class mPulse {
        if ($wgmPulseApiKey !== '') {
            $script = <<<EOD
 (function(){
-  if (window.BOOMR && window.BOOMR.version) { return; }
-  var dom,doc,where,iframe = document.createElement("iframe"),win = window;
+  // Boomerang Loader Snippet version 10
+  if (window.BOOMR && (window.BOOMR.version || window.BOOMR.snippetExecuted)) {
+    return;
+  }
+
+  window.BOOMR = window.BOOMR || {};
+  window.BOOMR.snippetExecuted = true;
+
+  var dom, doc, where, iframe = document.createElement("iframe"), win = window;
 
   function boomerangSaveLoadTime(e) {
-    win.BOOMR_onload=(e && e.timeStamp) || new Date().getTime();
+    win.BOOMR_onload = (e && e.timeStamp) || new Date().getTime();
   }
+
   if (win.addEventListener) {
     win.addEventListener("load", boomerangSaveLoadTime, false);
   } else if (win.attachEvent) {
@@ -26,27 +34,31 @@ class mPulse {
   }
 
   iframe.src = "javascript:void(0)";
-  iframe.title = ""; iframe.role = "presentation";
+  iframe.title = "";
+  iframe.role = "presentation";
   (iframe.frameElement || iframe).style.cssText = "width:0;height:0;border:0;display:none;";
   where = document.getElementsByTagName("script")[0];
   where.parentNode.insertBefore(iframe, where);
 
   try {
     doc = iframe.contentWindow.document;
-  } catch(e) {
+  } catch (e) {
     dom = document.domain;
-    iframe.src="javascript:var d=document.open();d.domain='"+dom+"';void(0);";
+    iframe.src = "javascript:var d=document.open();d.domain='" + dom + "';void(0);";
     doc = iframe.contentWindow.document;
   }
+
   doc.open()._l = function() {
     var js = this.createElement("script");
-    if (dom) { this.domain = dom; }
+    if (dom) {
+      this.domain = dom;
+    }
     js.id = "boomr-if-as";
     js.src = '//c.go-mpulse.net/boomerang/{{API_KEY}}';
-    BOOMR_lstart=new Date().getTime();
+    BOOMR_lstart = new Date().getTime();
     this.body.appendChild(js);
   };
-  doc.write('<body onload="document._l();"' + String.fromCharCode(62));
+  doc.write('<bo' + 'dy onload="document._l();"' + String.fromCharCode(62));
   doc.close();
 })();
 EOD;
